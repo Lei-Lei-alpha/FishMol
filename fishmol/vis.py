9a45c0c296d9a7b4fa67fe072fa695a1e2aebc45
Lei Lei <kodoralph@gmail.com>
2023-02-09 13:17:39 +0000

    Added funcs.py & modified vis.py

--- fishmol/vis.py
+++ fishmol/vis.py
@@ -1,6 +1,23 @@
 from ase import Atoms as ase_Atoms
 from ase.visualize import view as ase_view
+from IPython.display import HTML, display
+from tempfile import NamedTemporaryFile
 
 def view(atoms, viewer = "ngl"):
     atoms = ase_Atoms(symbols = atoms.symbs, positions = atoms.pos, cell = atoms.cell.lattice)
     ase_view(atoms)
+
+def render_atoms(atoms):
+    """
+    render the ase atoms object.
+    A: centre view
+    E: rotate
+    Double click on atom to bring the target atom into centre of view
+    Roll to zoom in/out
+    """
+    with NamedTemporaryFile('r+', suffix='.html') as ntf:
+        atoms.write(ntf.name, format='html')
+        ntf.seek(0)
+        html = ntf.read()
+        display(HTML(html))
+    return None
